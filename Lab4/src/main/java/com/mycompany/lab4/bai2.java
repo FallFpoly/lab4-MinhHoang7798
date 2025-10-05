5835ece3a30d5d2722f079d0a493707467123ae6
Kuro <Kuro@Kuro.lan>
2025-10-05 23:41:01 +0700

    Nộp bài Lab4

--- Lab4/src/main/java/com/mycompany/lab4/bai2.java
+++ Lab4/src/main/java/com/mycompany/lab4/bai2.java
@@ -9,8 +9,23 @@ package com.mycompany.lab4;
  * @author ICT
  */
 public class bai2 {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        bai1 sp1 = new bai1();
+        bai1 sp2 = new bai1();
+
+        
+        System.out.println("Nhap thong tin SP 1:");
+        sp1.nhap();
+
+        System.out.println("\nNhap thong tin SP 2:");
+        sp2.nhap();
+
+       
+        System.out.println("\n--- Thong tin SP 1 ---");
+        sp1.xuat();
+
+        System.out.println("\n--- Thong tin SP 2 ---");
+        sp2.xuat();
     }
 }
--- Lab4/src/main/java/com/mycompany/lab4/bai3.java
+++ Lab4/src/main/java/com/mycompany/lab4/bai3.java
@@ -4,10 +4,72 @@
  */
 package com.mycompany.lab4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
  */
 public class bai3 {
-    
+
+    private String tenSp;
+    private double donGia;
+    private double giamGia;
+
+    public bai3(String tenSp, double donGia, double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public bai3(String tenSp, double donGia) {
+        this(tenSp, donGia, 0);
+    }
+
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ten SP: ");
+        this.tenSp = sc.nextLine();
+
+        System.out.print("Don gia: ");
+        this.donGia = sc.nextDouble();
+
+        System.out.print("Giam gia: ");
+        this.giamGia = sc.nextDouble();
+    }
+
+    private double getThueNhapKhau() {
+        return this.donGia * 0.1;
+    }
+
+    public void xuat() {
+        System.out.println("Ten SP: " + this.tenSp);
+        System.out.println("Don gia: " + this.donGia);
+        System.out.println("Giam gia: " + this.giamGia);
+        System.out.println("Thue nhap khau: " + getThueNhapKhau());
+    }
+
+    public String getTenSp() {
+        return this.tenSp;
+    }
+
+    public void setTenSp(String tenSp) {
+        this.tenSp = tenSp;
+    }
+
+    public double getDonGia() {
+        return this.donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public double getGiamGia() {
+        return this.giamGia;
+    }
+
+    public void setGiamGia(double giamGia) {
+        this.giamGia = giamGia;
+    }
 }
